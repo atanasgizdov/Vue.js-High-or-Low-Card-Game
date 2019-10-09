4fb5cdc97138678627f1038ca89b87910897da8d
Atanas Gizdov <naskogizdov@yahoo.com>
2019-10-09 14:47:00 -0400

    final touches

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Card Game - Guess the number!</h1>
+    <div v-if = "cardValues.gameRunning == true">
+      <button class = "button" v-on:click="submitGuess(2)"> The number is Even</button>
+      <button class = "button" v-on:click="submitGuess(1)"> The number is Odd </button>
+    </div>
+    <div v-else>
+      <button v-on:click="restartGame()">Play Again</button>
+    </div>
+    <Card :cardValues="cardValues"/>
+    <br>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      cardValues:
+        {
+        gameRunning: true,
+        numberOfWins: 0,
+        cardValue: Math.floor(Math.random() * Math.floor(11)),
+        cardMessage: null
+        },
+    }
+  },
+  methods: {
+
+    submitGuess: function (selection) {
+      console.log ((this.cardValues.cardValue % 2))
+      if (selection == 1 && ((this.cardValues.cardValue % 2) > 0)) {
+        this.cardValues.cardMessage = "Congrats! You guessed correctly!"
+        this.cardValues.numberOfWins++
+      }
+      else if (selection == 2 && ((this.cardValues.cardValue % 2) == 0)) {
+        this.cardValues.cardMessage = "Congrats! You guessed correctly!"
+        this.cardValues.numberOfWins++
+      }
+      else {
+        this.cardValues.cardMessage = "Sorry, you did not guess correctly"
+      }
+      this.cardValues.gameRunning = false;
+    },
+
+    restartGame: function () {
+      this.cardValues.gameRunning = true
+      this.cardValues.cardValue = Math.floor(Math.random() * Math.floor(11))
+    },
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.small-container {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+}
+
+.button {
+  margin-left: 5px;
+  margin-right: 5px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <img v-if= "cardValues.gameRunning == true" src="https://i.imgur.com/TC1UgEj.jpg" alt="Avatar" style="width:100%">
+    <div v-else class="container">
+      <h4><b> {{ cardValues.cardMessage }}</b></h4>
+      <h4><b>The value of the card was : {{ cardValues.cardValue }}</b></h4>
+      <h5>You have won a total number of <b> {{ cardValues.numberOfWins }}</b> times</h5>
+      <p>Click play again to restart the game</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'card-class',
+    props: {
+      cardValues: Object
+    },
+  }
+</script>
+
+
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 60%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+
+</style>
